b8699b96428bcad14b27f1f06d566aa8eeb5847c
ankush <Mail2ankushd@gmail.com>
2024-11-21 20:22:30 +0530

    Surveys done???

--- src/main/java/com/ankush/EDI/Domain/Surveys/SurveyController.java
+++ src/main/java/com/ankush/EDI/Domain/Surveys/SurveyController.java
@@ -7,52 +7,54 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("java/api/user/survey")
 public class SurveyController {
     JdbcTemplate template;
-
-    JDBCInsert<Survey> insert;
-    JDBCInsert<SurveyAnswer> answerInsert;
-
-    JDBCRowMapper<Survey> mapper;
-    JDBCRowMapper<SurveyAnswer> answerMapper;
+    RowMapper<Survey> surveyRowMapper;
 
 
     public SurveyController(@Autowired JdbcTemplate template) {
         this.template = template;
-        insert = new JDBCInsert<>("surveys", Survey.class, template);
-        answerInsert = new JDBCInsert<>("survey_answers", SurveyAnswer.class, template);
-        mapper=new JDBCRowMapper<>(Survey.class,template);
-        answerMapper=new JDBCRowMapper<>(SurveyAnswer.class,template);
+        this.surveyRowMapper = (rs, rowNum) -> {
+            Survey s = new Survey();
+            s.setSurveyID(rs.getInt("surveyID"));
+            s.setAnswers(Arrays.stream(rs.getString("answers").split(",")).map(Integer::parseInt).toList());
+            s.setTimestamp(rs.getTimestamp("timestamp"));
+            return s;
+        };
     }
 
     @PostMapping("post")
     public ResponseEntity<?> post(@RequestBody @Valid Survey survey) throws IllegalAccessException {
         User user = Utils.getUser();
-        survey.setUserid(user.getUsername());
-        return ResponseEntity.ok(survey.insert(insert, answerInsert));
+        survey.compute(user.getUsername(), true);
+        survey.insert(template);
+        return ResponseEntity.ok(survey);
     }
 
     @GetMapping("get")
     public ResponseEntity<?> get(@RequestParam int id) {
         User user = Utils.getUser();
-        Survey s = template.queryForObject("select * from surveys where id=? and userid=?", mapper, id, user.getUsername());
-        s.setUserid(user.getUsername());
-        s.setAnswers(template.query("select * from survey_answers where surveyID=?", answerMapper, s.getId()));
+        Survey s = template.queryForObject("select * from surveys where surveyID=? and userid=?", surveyRowMapper, id, user.getUsername());
+        s.compute(user.getUsername(), false);
         return ResponseEntity.ok(s);
-
     }
 
-    @GetMapping("getAll")
+    @GetMapping("getRecent")
     public ResponseEntity<?> getAll() {
         User user = Utils.getUser();
-        List<Survey> s = template.query("select * from surveys where userid=?", mapper, user.getUsername());
+        List<Survey> s = template.query("select * from surveys where userid=? order by timestamp desc limit 3", surveyRowMapper, user.getUsername());
+        s.forEach(x -> x.compute(user.getUsername(), false));
         return ResponseEntity.ok(s);
     }
 
